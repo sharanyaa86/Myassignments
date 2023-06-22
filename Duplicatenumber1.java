41d3f473b06e00afe5ade79bd0ce990c0a05aa6c
sharanyaa86 <137065102+sharanyaa86@users.noreply.github.com>
2023-06-22 09:14:51 +0530

    Add files via upload

---
+++ Duplicatenumber1.java
@@ -0,0 +1,21 @@
+package week1.day3;
+
+import java.util.Arrays;
+
+public class Duplicatenumber1 {
+	
+public static void main(String[] args) {
+	int[] num = {2, 5, 7, 7, 5, 9, 2, 3};
+	
+	Arrays.sort(num);
+	
+	for (int i = 0; i<num.length-1; i++)
+	{
+	if(num[i]==num[i+1])
+	
+		System.out.println(num[i]);
+		
+	}
+	
+}
+}
