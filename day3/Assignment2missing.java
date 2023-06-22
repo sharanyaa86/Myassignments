f946312f23584aae6bf97111611efd08f1596195
sharanyaa86 <137065102+sharanyaa86@users.noreply.github.com>
2023-06-22 09:23:00 +0530

    Add files via upload

---
+++ day3/Assignment2missing.java
@@ -0,0 +1,19 @@
+package week1.day3;
+
+import java.util.Arrays;
+
+public class Assignment2missing {
+public static void main(String[] args) {
+	int[] arr = {1,2,3,4,7,6,8};
+	
+	Arrays.sort(arr);
+	
+	for (int i = 0; i < arr.length; i++) {
+		if (i+1!= arr[i]);
+		System.out.println("Missing element:" + (i+1));
+		break;
+		
+	}
+	
+}
+}
